f5b81183fd6351af8a5f31eba67ce0959ef16eba
Emma Egstad <emma@egstad.com>
2021-08-26 16:10:12 -0700

    created question UI, event listener on submit button

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// import functions
-
 // reference needed DOM elements
-
+const textArea = document.querySelector('.question-area');
+const submitButton = document.querySelector('.question-button');
 
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+submitButton.addEventListener('click', () => {
+
+});
