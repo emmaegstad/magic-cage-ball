1b331d750097e43a2248a03eed62a3ac7de2e7df
Emma Egstad <emma@egstad.com>
2021-08-26 16:28:37 -0700

    added quote array, created random quote generator

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// reference needed DOM elements
 const textArea = document.querySelector('.question-area');
 const submitButton = document.querySelector('.question-button');
+let answer = document.querySelector('.answer');
 
-// set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+const cageQuotes = [
+    `I think I jump around more when I'm alone.`,
+    `It's a family that's loaded with grudges and passion. We come from a long line of robbers and highwaymen in Italy, you know. Killers, even.`,
+    `Sometimes people think I'm wearing a wig when I'm not wearing a wig, and then sometimes they think I'm not wearing a wig when I am wearing a wig.`,
+    `I am not a demon. I am a lizard, a shark, a heat-seeking panther. I want to be Bob Denver on acid playing the accordion.`,
+    `I don't drink blood, and last time I looked in the mirror, I had a reflection.`,
+    `I'm not afraid to play ugly - look at 'Adaptation.' I looked like a turd that a cat had coughed up.`,
+    `I'm sticking my tongue out in scenes to try to make that work in 3D. I'm thinking I'll try to get my tongue all the way out to the second row of the audience.`];
 
 submitButton.addEventListener('click', () => {
-
+    const min = Math.ceil(0);
+    const max = Math.floor(6);
+    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    answer.textContent = cageQuotes[randomNum];
 });
